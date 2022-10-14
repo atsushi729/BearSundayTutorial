0d181ee7a1e3dc04504e3e6705d8d8fdbd433027
Atsushi <atu729@icloud.com>
2022-10-14 23:42:34 +0900

    added exception

--- src/Resource/App/Weekday.php
+++ src/Resource/App/Weekday.php
@@ -6,15 +6,26 @@ namespace MyVendor\Weekday\Resource\App;
 
 use BEAR\Resource\ResourceObject;
 use DateTimeImmutable;
+use MyVendor\Weekday\Exception\InvalidDateTimeException;
 
 class Weekday extends ResourceObject
 {
     public function onGet(int $year, int $month, int $day): static
     {
         $dateTime = DateTimeImmutable::createFromFormat('Y-m-d', "$year-$month-$day");
+        if (! $dateTime instanceof DateTimeImmutable) {
+            throw new InvalidDateTimeException("$year-$month-$day");
+        }
+
         $weekday = $dateTime->format('D');
         $this->body = ['weekday' => $weekday];
 
         return $this;
+    }
+
+    public function tesInvalidDateTime(): void
+    {
+        $this->expectException(InvalidDateTimeException::class);
+        $this->resource->get('app://self/weekday', ['year' => '-1', 'month' => '1', 'day' => '1']);
     }
 }
